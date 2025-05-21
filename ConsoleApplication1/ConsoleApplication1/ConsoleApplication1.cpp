f8bbbbea576da4706830d87516c9a6d4b5a0c3c0
Ktoto112 <19.12.2006sasha@gmail.com>
2025-05-21 20:45:21 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+
+template <typename T>
+T gcd(T a, T b) {
+    while (b != 0) {
+        T temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int x = 36, y = 60;
+    cout << "GCD(" << x << ", " << y << ") = " << gcd(x, y) << endl;
+
+    
+    long long a = 123456789, b = 987654321;
+    cout << "GCD(" << a << ", " << b << ") = " << gcd(a, b) << endl;
+
+    return 0;
+}
